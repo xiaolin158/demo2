2ff18c920d0ce7bcca886dbb8e7ab7ed36cd6d3f
offcn <1412542326@qq.com>
2019-08-27 20:29:49 +0800

    更新

--- src/main/java/com/offcn/demo2/controller/HolleoController.java
+++ src/main/java/com/offcn/demo2/controller/HolleoController.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Controller;
  * @Created by Administrator
  */
 
-@rescc
+@Controller
 public class HolleoController {
+    public static void main(String[] args) {
+        System.out.println("sdfsdfdsf");
+    }
 
 
 }
